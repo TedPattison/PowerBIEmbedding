39d7233ca5e267d9a6d0b099ebf797a536a0365b
Ted Pattison <ted@tedpattison.net>
2020-04-24 08:23:21 -0400

    Updates

--- Demos/RegisterPowerBiPublicClient.ps1
+++ Demos/RegisterPowerBiPublicClient.ps1
@@ -8,53 +8,35 @@ $credential = New-Object –TypeName System.Management.Automation.PSCredential `
 
 $authResult = Connect-AzureAD # -Credential $credential
 
-
-# display name for new public client app
-$appDisplayName = "Power BI Public Client"
+# authenticate with Azure AD
+$authResult = Connect-AzureAD 
 
 # get user account ID for logged in user
 $user = Get-AzureADUser -ObjectId $authResult.Account.Id
 
-# get tenant name of logged in user
-$tenantName = $authResult.TenantDomain
+# add variables for applicaiton name and redirect URI
+$appDisplayName = "Power BI Public Client"
+$redirectUri = "http://localhost/app1234"
 
-# create Azure AD Application
-$replyUrl = "http://localhost/app1234"
 
 $aadApplication = New-AzureADApplication `
                         -DisplayName $appDisplayName `
                         -PublicClient $true `
                         -AvailableToOtherTenants $false `
-                        -ReplyUrls @($replyUrl)
+                        -ReplyUrls @($redirectUri)
+
+# retrieve application Id
+$appId = $aadApplication.AppId
 
 # create service principal for application
-$appId = $aadApplication.AppId
 $serviceServicePrincipal = New-AzureADServicePrincipal -AppId $appId
 
 # assign current user as application owner
 Add-AzureADApplicationOwner -ObjectId $aadApplication.ObjectId -RefObjectId $user.ObjectId
 
-# configure delegated permisssions for the Power BI Service API
-$requiredAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
-$requiredAccess.ResourceAppId = "00000009-0000-0000-c000-000000000000"
-
-# create first delegated permission - Report.Read.All
-$permission1 = New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" `
-                          -ArgumentList "4ae1bf56-f562-4747-b7bc-2fa0874ed46f","Scope"
-
-# create second delegated permission - Dashboards.Read.All
-$permission2 = New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" `
-                          -ArgumentList "2448370f-f988-42cd-909c-6528efd67c1a","Scope"
-
-# add permissions to ResourceAccess list
-$requiredAccess.ResourceAccess = $permission1, $permission2
-
-# add permissions by updating application with RequiredResourceAccess object
-Set-AzureADApplication -ObjectId $aadApplication.ObjectId -RequiredResourceAccess $requiredAccess
-
+# generate text file that contains application Id and redirect URI
 $outputFile = "$PSScriptRoot\PowerBiPublicClient.txt"
 Out-File -FilePath $outputFile -InputObject "--- Power BI Public Client ---"
 Out-File -FilePath $outputFile -Append -InputObject "application-id: $appId"
 Out-File -FilePath $outputFile -Append -InputObject "redirect-uri: $replyUrl"
-
 Notepad $outputFile
